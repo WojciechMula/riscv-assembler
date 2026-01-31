410848b77c9eba0634416159ecb6a930aa43c4b9
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2026-01-31 16:54:41 +0100

    Add script that obtains the input for generator

---
+++ assets/README.rst
@@ -0,0 +1,12 @@
+================================================================================
+                Obtaining input for the generator
+================================================================================
+
+Please run `./prepare.sh` (or see what's there).
+
+Prerequirements:
+
+* checked out https://github.com/riscv/sail-riscv/
+* run `./build_simulators.sh` in the `sail-riscv` project to create
+  configuration files
+* installed `sail` binary, it should be available in your path
---
+++ assets/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ ${RISCV_SAIL} == "" ]]
+then
+    echo "Please define RISCV_SAIL pointing the sources of https://github.com/riscv/sail-riscv/"
+    exit 1
+fi
+
+config=${RISCV_SAIL}/build/config/rv64d_v128_e64.json
+
+if [[ ! -f ${config} ]]
+then
+    echo "${config} not found, make sure you run ./build_simulators.sh in ${RISCV_SAIL}"
+fi
+
+set -e
+set -x
+
+eval $(opam env)
+
+sail --project ${RISCV_SAIL}/model/riscv.sail_project \
+     --all-modules \
+     --output-sail \
+     --config ${config}
